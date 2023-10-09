80411026e0409932e8496e0a1b41e58596f93ff7
Michal Stehlík <michal.stehlik@pslib.cz>
2023-10-09 16:04:44 +0200

    cleaner solution

--- r01-intro/src/App.tsx
+++ r01-intro/src/App.tsx
@@ -7,13 +7,20 @@ import Counter from './components/Counter'
 const App = () => {
   const [a, setA] = useState<number>(0);
   const [b, setB] = useState<number>(0);
+  
+  const AddA = (x: number) => {
+    setA(y => y + x);
+  }
+  const AddB = (x: number) => {
+    setB(y => y + x);
+  }
   return(
     <>
       <Container number="2" text="t">AAAA<span>BBBB</span></Container>
       <Greetings text="Nazdar" />
-      <Counter value={a} setValue={setA} /> 
+      <Counter value={a} setValue={AddA} /> 
       + 
-      <Counter value={b} setValue={setB}/> 
+      <Counter value={b} setValue={AddB}/> 
       = 
       {a + b}
     </>
--- r01-intro/src/components/Counter.tsx
+++ r01-intro/src/components/Counter.tsx
@@ -1,14 +1,15 @@
 const Counter = (
     // {value, setValue}: {value: number, setValue: React.Dispatch<React.SetStateAction<number>>}
-    // { value, setValue } : {value: number,  setValue: { (value: number): void; (value: (prev: number) => number): void; }; }
-    { value, setValue } : {value: number,  setValue: (x: ((val:number) => number) | number) => void}
+    // { value, setValue } : {value: number,  setValue: { (value: (prev: number) => number): void; (value: number): void}; }
+    // { value, setValue } : {value: number,  setValue: (x: ((val:number) => number) | number) => void}
+    {value, setValue}: {value: number, setValue: (x: number) => void}
     ) => {
     return (
         <span>
             <button onClick={e => {setValue(0)}}>0</button>
-            <button onClick={e => {setValue(value - 1)}}>-</button>
+            <button onClick={e => {setValue(-1)}}>-</button>
             <span>{value}</span>
-            <button onClick={e => {setValue(x => x + 1)}}>+</button>
+            <button onClick={e => {setValue(1)}}>+</button>
         </span>
     );
 }
