3aad4c51b419fadbb8f3418b05b7c66092f59fc5
Michal Stehlík <michal.stehlik@pslib.cz>
2023-10-09 15:45:05 +0200

    type safe setValue

--- r01-intro/src/App.tsx
+++ r01-intro/src/App.tsx
@@ -1,14 +1,21 @@
+import {useState} from "react"
 import './App.css'
 import Greetings from "./components/Greetings"
 import Container from "./components/Container"
 import Counter from './components/Counter'
 
 const App = () => {
+  const [a, setA] = useState<number>(0);
+  const [b, setB] = useState<number>(0);
   return(
     <>
-    <Container number="2" text="t">AAAA<span>BBBB</span></Container>
-    <Greetings text="Nazdar" />
-    <Counter />
+      <Container number="2" text="t">AAAA<span>BBBB</span></Container>
+      <Greetings text="Nazdar" />
+      <Counter value={a} setValue={setA} /> 
+      + 
+      <Counter value={b} setValue={setB}/> 
+      = 
+      {a + b}
     </>
   );
   
--- r01-intro/src/components/Counter.tsx
+++ r01-intro/src/components/Counter.tsx
@@ -1,10 +1,12 @@
-import {useState} from "react"
-
-const Counter = () => {
-    const [value, setValue] = useState<number>();
+const Counter = (
+    // {value, setValue}: {value: number, setValue: React.Dispatch<React.SetStateAction<number>>}
+    // { value, setValue } : {value: number,  setValue: { (value: number): void; (value: (prev: number) => number): void; }; }
+    { value, setValue } : {value: number,  setValue: (x: ((val:number) => number) | number) => void}
+    ) => {
     return (
         <span>
-            <button onClick={e => {setValue(3)}}>-</button>
+            <button onClick={e => {setValue(0)}}>0</button>
+            <button onClick={e => {setValue(value - 1)}}>-</button>
             <span>{value}</span>
             <button onClick={e => {setValue(x => x + 1)}}>+</button>
         </span>
@@ -12,3 +14,5 @@ const Counter = () => {
 }
 
 export default Counter;
+// type UseState<S> = (action: S | ((prevState: S) => S)) => void;
+// 
